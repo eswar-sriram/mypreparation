795c1d3bfec290c3c28023d5a77753a7de0fbd77
eswar <eswarsriram17@gmail.com>
2022-07-05 16:51:01 +0530

    Initial commit

---
+++ src/main/java/com/project/mypreparation/MypreparationApplication.java
@@ -0,0 +1,13 @@
+package com.project.mypreparation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MypreparationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MypreparationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/mypreparation/MypreparationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.mypreparation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MypreparationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
